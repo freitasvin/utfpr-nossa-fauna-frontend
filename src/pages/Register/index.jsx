a7c8aa4f261a2889b528108fc6169971aefdb390
nevitonvelho <nevitonvelho01@gmail.com>
2022-12-09 23:44:52 -0300

    Novo

--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -1,17 +1,167 @@
 import { Container } from "@mui/system"
-import { FullPage } from "../../components/Wrappers"
-import {HeaderRegister} from "./styles"  
+import {HeaderRegister,ContainerRegister, FormRegister, GridRegister, SelectRegister, FooterRegister} from "./styles"  
+import { TextMedium, TextLight } from "../../components/Texts"
+import { Grid, TextField, InputBase } from "@mui/material"
+import {Header} from '../../components/Header'
 
 export const Register = () => {
     return (
         <>
-           <FullPage>
-                <Container >
+            <Header/>
+            <ContainerRegister>
+                <Container>
                     <HeaderRegister>
-                        sss
+                        <TextMedium>CADASTRO</TextMedium>
                     </HeaderRegister>
                 </Container>
-           </FullPage>
+            </ContainerRegister>
+
+            <FormRegister>
+                <Container>
+
+                    <GridRegister>
+
+                        <InputBase
+                            id="outlined-basic" 
+                            label="Nome"
+                            placeholder="Nome"
+                            variant="outlined" 
+                            sx={
+                                {
+                                    backgroundColor: '#fff',
+                                    borderRadius: '999px',
+                                    border: 'none',
+                                    padding: '8px 22px'
+                                }
+                            }
+                        />
+
+                        <InputBase
+                            id="outlined-basic" 
+                            label="Nome Cientifico"
+                            placeholder="Nome Cientifico"
+                            variant="outlined" 
+                            sx={
+                                {
+                                    backgroundColor: '#fff',
+                                    borderRadius: '999px',
+                                    border: 'none',
+                                    padding: '8px 22px'
+                                }
+                            }
+                        />
+
+                       
+                            <SelectRegister>
+                                <option value="0">Tipo 1</option>
+                                <option value="1">Tipo 2</option>
+                                <option value="2">Tipo 3</option>
+                                <option value="3">Tipo 4</option>
+                            </SelectRegister>
+
+                        <InputBase
+                            id="outlined-basic" 
+                            placeholder="Haitat"
+                            variant="outlined" 
+                            sx={
+                                {
+                                    backgroundColor: '#fff',
+                                    borderRadius: '999px',
+                                    border: 'none',
+                                    padding: '8px 22px'
+                                }
+                            }
+                        />
+
+                        <InputBase
+                            id="outlined-basic" 
+                            placeholder="Alimentação"
+                            variant="outlined" 
+                            sx={
+                                {
+                                    backgroundColor: '#fff',
+                                    borderRadius: '999px',
+                                    border: 'none',
+                                    padding: '8px 22px'
+                                }
+                            }
+                        />  
+
+                        <InputBase
+                            id="outlined-basic" 
+                            placeholder="Reprodução"
+                            variant="outlined" 
+                            sx={
+                                {
+                                    backgroundColor: '#fff',
+                                    borderRadius: '999px',
+                                    border: 'none',
+                                    padding: '8px 22px'
+                                }
+                            }
+                        /> 
+
+                        <InputBase
+                            id="outlined-basic" 
+                            placeholder="Caracteristicas"
+                            variant="outlined" 
+                            sx={
+                                {
+                                    backgroundColor: '#fff',
+                                    borderRadius: '999px',
+                                    border: 'none',
+                                    padding: '8px 22px'
+                                }
+                            }
+                        />  
+
+                        <InputBase
+                            id="outlined-basic" 
+                            placeholder="Importancias ecologicas"
+                            variant="outlined" 
+                            sx={
+                                {
+                                    backgroundColor: '#fff',
+                                    borderRadius: '999px',
+                                    border: 'none',
+                                    padding: '8px 22px'
+                                }
+                            }
+                        />  
+                    </GridRegister>
+
+
+                    <FooterRegister>
+                        <div className="sub-file">
+                            <div className="teste">
+                                <h4>Fotos:</h4>
+                                <div class="image-input">
+                                    <input type="file" accept="image/*" id="imageInput"/>
+                                    <label for="imageInput" class="image-button">
+                                        <img src="https://cdn-icons-png.flaticon.com/512/1829/1829415.png" class="image-preview"/>
+                                    </label>
+                                    
+                                </div>
+                            </div>
+
+                            <div className="teste">
+                                <h4>Pegadas:</h4>
+                                <div class="image-input">
+                                    <input type="file" accept="image/*" id="imageInput"/>
+                                    <label for="imageInput" class="image-button">
+                                        <img src="https://cdn-icons-png.flaticon.com/512/1829/1829415.png" class="image-preview"/>
+                                    </label>
+                                    
+                                </div>
+                            </div>
+                        </div>
+                        <div className="btn-cad">
+                            <button>CADASTRAR</button>
+                        </div>
+
+                    </FooterRegister>
+                </Container>
+            </FormRegister>
         </>
     )
 }
--- src/pages/Register/styles.js
+++ src/pages/Register/styles.js
@@ -1,5 +1,84 @@
 import styled from "styled-components"
 
+
+export const ContainerRegister = styled.div`
+    border-radius:0 0 22px 22px;
+    background-color: #134720;
+    width: 100%;
+    top: 0;
+`
+
 export const HeaderRegister = styled.div`
-    background-color: red;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 22px;
+`
+
+export const FormRegister = styled.form`
+    background-color: #79B158;
+    height: 100vh;
+    padding: 42px 0;
+`
+
+export const GridRegister = styled.form`
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: 2rem;
+`
+
+export const SelectRegister = styled.select`
+    border-radius: 999px;
+    border: none;
+    padding: 8px 22px;
+    background-color: #fff;
+`
+
+
+export const FooterRegister = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    align-items: center;
+    gap: 2rem;
+
+    button{
+        margin-left: 0 auto;
+        background-color: #134720;
+        color: #fff;
+        border: none;
+        padding: 18px;
+        border-radius: 22px;
+    }
+
+    .btn-cad{
+        right: 0;
+        margin-top: 42px;
+    }
+
+    .sub-file{
+        display: flex;
+        gap: 2rem;
+    }
+
+    .teste{
+       input{
+        display: none;
+       }
+
+        .image-input{
+            width: 100px;
+            height: 100px;
+            display: flex;
+            border-radius: 22px;
+            padding: 32px;
+            align-items: center;
+            justify-content: center;
+            background-color: #ffff;
+            
+
+            img{
+                width: 72px;
+            }
+        }
+    }
 `
